5678b9904ebb0e9c191daa0a01d4dcefefaa706a
enzo.juhel <enzo.juhel@epita.fr>
2026-05-09 22:16:28 +0200

    Added Pi-hole

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 # ── Load env ──────────────────────────────────────────────
-source /home/enzo/homeserver/.env
+source "/home/$USER/homeserver/.env"
 
 ENZO_UID=1000
 ENZO_GID=1000
@@ -12,8 +12,22 @@ echo "[+] Creating directory tree..."
 dirs=(
   "$DATA_PATH/immich/upload"
   "$DATA_PATH/immich/postgres"
+  "$DATA_PATJ/pihole/etc-pihole"
 )
 
 for d in "${dirs[@]}"; do
   mkdir -p "$d"
 done
+
+ln -s "/home/$USER/homerserver/.env" immich/.env
+ln -s "/home/$USER/homerserver/.env" traefik/.env
+ln -s "/home/$USER/homerserver/.env" monitoring/.env
+ln -s "/home/$USER/homerserver/.env" pihole/.env
+
+# TODO: Install tlp, edit /etc/tlp.conf: 
+# START_CHARGE_THRESH_BAT0=75
+# STOP_CHARGE_THRESH_BAT0=80
+# TLP_DEFAULT_MODE=BAL
+# TLP_PERSISTENT_DEFAULT=1
+# sudo tlp setcharge 80 1 BAT0
+
